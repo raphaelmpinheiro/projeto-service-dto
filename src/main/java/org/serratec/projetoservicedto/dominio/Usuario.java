ebe722ac083575d41693f925b166f4edbbd5c1ac
Raphael Pinheiro <raphaelmpinheiro@gmail.com>
2021-11-05 19:46:12 -0300

    Criando a classe usuario.

---
+++ src/main/java/org/serratec/projetoservicedto/dominio/Usuario.java
@@ -0,0 +1,69 @@
+package org.serratec.projetoservicedto.dominio;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_usuario")
+	private long id;
+	
+	private String nome;
+	private String email;
+	private String senha;
+				
+	public Usuario() {
+		
+	}
+	
+	public Usuario(long id, String nome, String email, String senha) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.senha = senha;
+	}
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		return id == other.id;
+	}		
+}
